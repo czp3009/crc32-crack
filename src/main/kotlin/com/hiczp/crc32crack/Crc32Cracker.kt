39de75aeb32b45c5d38f3d642e08cb4a8e892f66
czp3009 <czp3009@gmail.com>
2019-03-03 17:01:40 +0800

    init

--- src/main/kotlin/com/hiczp/crc32crack/Crc32Cracker.kt
+++ src/main/kotlin/com/hiczp/crc32crack/Crc32Cracker.kt
@@ -2,6 +2,9 @@ package com.hiczp.crc32crack
 
 import java.util.*
 
+/**
+ * https://github.com/zacyu/bilibili-helper/blob/master/src/js/libs/crc32.js
+ */
 object Crc32Cracker {
     private const val CRC32_POLYNOMIAL = 0xedb88320.toInt()
 
@@ -17,23 +20,126 @@ object Crc32Cracker {
         crc
     }
 
-    private val REVERSE_TABLE = IntArray(256) {
-        CRC_TABLE.indexOfFirst { crcTableElement ->
-            crcTableElement ushr 24 == it
+    private val rainbowTableHash = IntArray(100000)
+    private val rainbowTableValue = IntArray(100000)
+    private val shortHashBucketStarts: IntArray
+
+    init {
+        val fullHashCache = IntArray(100000)
+        val shortHashBuckets = IntArray(65537)
+        // Initialize the rainbow Table
+        repeat(100000) {
+            val hash = compute(it)
+            fullHashCache[it] = hash
+            shortHashBuckets[hash ushr 16]++
+        }
+        var runningSum = 0
+        shortHashBucketStarts = shortHashBuckets.mapInPlace {
+            runningSum += it
+            runningSum
+        }
+        repeat(100000) {
+            val index = --shortHashBucketStarts[fullHashCache[it] ushr 16]
+            rainbowTableHash[index] = fullHashCache[it]
+            rainbowTableValue[index] = it
         }
     }
 
-    fun crack(hash: Int, start: Int = 1, end: Int = 2000000000): List<Long> {
-        //get last 4 index
-        var leftOperand = hash
-        var rightOperand = 0xffffffff.toInt()
-        val last4index = IntArray(4) {
-            leftOperand = leftOperand xor rightOperand
-            REVERSE_TABLE[leftOperand ushr (3 - it) * 8].also { index ->
-                rightOperand = CRC_TABLE[index] ushr it * 8
+    private fun compute(input: Int, addPadding: Boolean = false): Int {
+        var crc = 0
+        input.toArray().forEach {
+            crc = update(crc, it)
+        }
+        if (addPadding) {
+            repeat(5) {
+                crc = update(crc, 0)
+            }
+        }
+        return crc
+    }
+
+    @Suppress("NOTHING_TO_INLINE")
+    private inline fun update(crc: Int, code: Int) =
+        crc ushr 8 xor CRC_TABLE[crc xor code and 0xff]
+
+    private fun lookup(hash: Int): List<Int> {
+        val candidates = LinkedList<Int>()
+        val shortHash = hash ushr 16
+        for (i in shortHashBucketStarts[shortHash] until shortHashBucketStarts[shortHash + 1]) {
+            if (rainbowTableHash[i] == hash) {
+                candidates.add(rainbowTableValue[i])
+            }
+        }
+        return candidates
+    }
+
+    @JvmStatic
+    fun crack(hash: Int): List<Int> {
+        val candidates = LinkedList<Int>()
+        val hashValue = hash.inv()
+        var baseHash = 0xffffffff.toInt()
+
+        for (digitCount in 1 until 10) {
+            baseHash = update(baseHash, 0x30)
+            if (digitCount < 6) {
+                // Direct lookup
+                candidates.addAll(lookup(hashValue xor baseHash))
+            } else {
+                // Lookup with prefix
+                val startPrefix = pow10(digitCount - 6)
+                val endPrefix = pow10(digitCount - 5)
+
+                for (prefix in startPrefix until endPrefix) {
+                    for (postfix in lookup(hashValue xor baseHash xor compute(prefix, true))) {
+                        candidates.add(prefix * 100000 + postfix)
+                    }
+                }
             }
         }
 
-        return Collections.emptyList()
+        return candidates
     }
+
+    @UseExperimental(ExperimentalUnsignedTypes::class)
+    @JvmStatic
+    fun crack(hash: String) = Crc32Cracker.crack(hash.toUInt(16).toInt())
 }
+
+//do transform on original array
+private inline fun IntArray.mapInPlace(transform: (Int) -> Int): IntArray {
+    for (i in indices) {
+        this[i] = transform(this[i])
+    }
+    return this
+}
+
+//java.lang.Integer
+private val sizeTable = intArrayOf(9, 99, 999, 9999, 99999, 999999, 9999999, 99999999, 999999999, Int.MAX_VALUE)
+
+//optimized int to array
+@Suppress("NOTHING_TO_INLINE")
+private inline fun Int.toArray(): IntArray {
+    var length = 0
+    while (true) {
+        if (this <= sizeTable[length]) {
+            length++
+            break
+        }
+        length++
+    }
+
+    val array = IntArray(length)
+    var number = this
+    for (i in array.lastIndex downTo 0) {
+        array[i] = number % 10
+        number /= 10
+    }
+
+    return array
+}
+
+//optimized 10.pow(n)
+private val powTable = intArrayOf(1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000)
+
+@Suppress("NOTHING_TO_INLINE")
+private inline fun pow10(n: Int) = powTable[n]
--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,10 +1,119 @@
 import com.hiczp.crc32crack.Crc32Cracker
 
+val hashes = arrayOf(
+    "766bd575",
+    "63ccd15d",
+    "fd2df0b3",
+    "ce0115d5",
+    "793a61e3",
+    "d743d139",
+    "cf8dd9bf",
+    "16b79618",
+    "8d45d74d",
+    "3f530220",
+    "3f530220",
+    "3ec02d3",
+    "abca552c",
+    "5f62afc0",
+    "fdbec380",
+    "29fa3a8b",
+    "f9f021dd",
+    "78c04697",
+    "92e03335",
+    "9d6ccb4c",
+    "160c0e39",
+    "8b6c54fc",
+    "ce4e8ab1",
+    "bd90c906",
+    "bd90c906",
+    "a749a95",
+    "f677602",
+    "51c279fc",
+    "7497f23d",
+    "9edda172",
+    "4d3b048f",
+    "191762ef",
+    "e5590223",
+    "dee851ea",
+    "b5220f3a",
+    "aabda2b1",
+    "58113f15",
+    "35fe3ccb",
+    "bff9f700",
+    "31782e70",
+    "28259456",
+    "966a14a3",
+    "383bde1e",
+    "7dc9e702",
+    "2a85a104",
+    "b179927c",
+    "311191ac",
+    "7f7593b6",
+    "987e8257",
+    "d1865cd4",
+    "f972be3c",
+    "1bdd0417",
+    "6c061ea8",
+    "aa0ad363",
+    "e6788939",
+    "e6788939",
+    "e6788939",
+    "2647628e",
+    "125b7a85",
+    "be3f4749",
+    "be3f4749",
+    "3acca374",
+    "346ef7f6",
+    "f06e8386",
+    "4bf0df6",
+    "992ef625",
+    "793a61e3",
+    "153c8193",
+    "153c8193",
+    "e7ac7686",
+    "a1340625",
+    "bd90c906",
+    "bd90c906",
+    "9342605b",
+    "6f7be8c0",
+    "6f7be8c0",
+    "ee7bcf3e",
+    "26a6e3b3",
+    "26a6e3b3",
+    "506420ee",
+    "c8d4ec03",
+    "bee1b11e",
+    "d3f57589",
+    "99d99038",
+    "99d99038",
+    "d4302f21",
+    "af81240a",
+    "b2b247ab",
+    "69adaf89",
+    "de32df62",
+    "fb6db529",
+    "fb6db529",
+    "fb6db529",
+    "fb6db529",
+    "eecede7f",
+    "ac733982",
+    "f5faf79e",
+    "9281297"
+)
+
+//test on i7-8700
 fun main() {
+    //307 ms
+    val initStart = System.currentTimeMillis()
+    val crc32Cracker = Crc32Cracker
+    val initEnd = System.currentTimeMillis()
+    println("Init finish in ${initEnd - initStart} ms")
+
+    //82 ms
     val start = System.currentTimeMillis()
-
-    println(Crc32Cracker.crack(0x822d8a7e.toInt()))
-
+    hashes.forEach {
+        println(crc32Cracker.crack(it))
+    }
     val end = System.currentTimeMillis()
-    println("Done in ${end - start} ms")
+    println("${hashes.size} hashes done in ${end - start} ms")
 }
